350783b36ad6f10042d552429f4857b390e0effd
xinrandf <xinrandee@gmail.com>
2025-07-29 14:33:15 -0700

    When all activities are deselected or unselect inquiry window pop open/close

--- js/activity.js
+++ js/activity.js
@@ -13,4 +13,35 @@ $(document).ready(function() {
     $("table tbody").on("click", "td.selectable", function() {
         $(this).toggleClass("selected");
     });
+
+    // User interaction with table cells
+    $("td").click(function() {
+        // Get content of cell
+        var content = $(this).text().trim();
+
+        // Check if content does not contain "Not Available"
+        if (content !== "Not Available") {
+            // Add or remove class when cell is selected
+            $(this).toggleClass("tdhighlight");
+
+            if ($(this).hasClass("tdhighlight")) {
+                // Make display box visible
+                $("#displaySelected").css("visibility", "visible");
+                $("#displaySelected").css("margin-top", "2em");
+
+                // Add child element with contents of cell
+                $("#result").append("<p>" + content + "</p>");
+            } else {
+                // Remove child element
+                $("#result p:contains('" + content + "')").remove();
+
+                // Check if there are any child elements within parent
+                if ($("#result").has("p").length === 0) {
+                    // Hide display box
+                    $("#displaySelected").css("visibility", "hidden");
+                    $("#displaySelected").css("margin-top", "0");
+                }
+            }
+        }
+    });
 });
